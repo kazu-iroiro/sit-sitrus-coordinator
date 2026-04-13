daf6845f3e22a7f44e8c165827e0c24404b4694b
kazu-iroiro <139434644+kazu-iroiro@users.noreply.github.com>
2026-04-14 02:49:19 +0900

    バージョン表記をv1.0.2に更新

--- src/content/content.js
+++ src/content/content.js
@@ -211,7 +211,7 @@ class SitrusCoordinator {
         // --- バージョン表記 ---
         const versionText = document.createElement('div');
         versionText.id = 'sc-version-text';
-        versionText.textContent = 'SITRUS Coordinator - v1.0.1';
+        versionText.textContent = 'SITRUS Coordinator - v1.0.2';
         document.body.appendChild(versionText);
 
         // --- 教職員向けトグルボタンとフォームの再配置 ---
